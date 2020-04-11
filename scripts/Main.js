fdbaba7d591fff44e73aac4daadf27f74d462a62
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-04-12 00:47:20 +1000

    please work

--- scripts/Main.js
+++ scripts/Main.js
@@ -61,9 +61,9 @@ function setup() {
 	resize();
 	manager = new SceneManager(canvas.getContext("2d", { alpha: false }));
 	if (window.ontouchstart) {
-		window.addEventListener("touchstart", fix);
-		window.addEventListener("touchmove", touch);
-		window.addEventListener("touchend", touch);
+		canvas.addEventListener("touchstart", fix);
+		canvas.addEventListener("touchmove", touch);
+		canvas.addEventListener("touchend", touch);
 	} else {
 		window.addEventListener("mousedown", fix);
 		window.addEventListener("mousemove", move);
